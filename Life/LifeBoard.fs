3fa80db9790d8f36b35c250ae98b9e251512a2ac
Christopher Roe <cpr@tweega.com>
2020-06-27 16:36:13 +0100

    working with edged and edgeless modes

--- Life/LifeBoard.fs
+++ Life/LifeBoard.fs
@@ -8,11 +8,16 @@ type CellInfo = (CellContents * CellContents) * int
 type LifeArray = array<CellInfo>
 type BoardDimensions = {rows: int; columns: int}
 
+type SurfaceType = 
+    | Edged //cells on the edge of the board have less than 8 neighbours
+    | Edgeless  // all cells have 8 neighbours
+
 type LifeBoard =
     {   
         dimensions: BoardDimensions        
         lifeArray: LifeArray        
-        changeFlag: bool // so that we can stop the timer if there are no changes      
+        changeFlag: bool // so that we can stop the timer if there are no changes 
+        surfaceType: SurfaceType
     }
 
 type Cartesian = int * int
@@ -45,33 +50,119 @@ let evolutionRules(kinCount, currentState) =
     else 
         EmptyCell   // more than three kins
     
+let stringifyIntSeq (s: string) (intSeq: seq<int>)= 
+    let strSeq = intSeq |> Seq.map(fun i -> i.ToString())
+    sprintf "%s" (String.concat s strSeq)
 
-let updateKinCount(lifeBoard: LifeBoard, index, cellType) = 
+let constant x = fun _ -> x
+
+let getEdgedBounds(index, rows, columns) =   //edged bounds means that cell 0 has only 3 neighbours(kin)  
+    let currentRow = int (index / columns)
+    
+    let IsInCurrentRowBounds = 
+        fun index -> index >= (currentRow * columns) && index <= (((currentRow + 1) * columns ) - 1)
+
+    let IsInPreviousRowBounds = 
+        if (currentRow + 1) < rows then 
+            fun(index) -> index >= ((currentRow + 1) * columns) && index <= (((currentRow + 2) * columns) - 1)
+        else 
+            constant false
+
+    let IsInNextRowBounds =        
+        if currentRow > 0 then 
+            fun(index) -> index >= ((currentRow - 1) * columns) && index <= ((currentRow * columns) - 1)            
+        else 
+            constant false
+    
+    (IsInPreviousRowBounds, IsInCurrentRowBounds, IsInNextRowBounds)
+
+
+let getKin rows columns surfaceType index : seq<int>= 
+    
+    match surfaceType with
+        | Edged -> 
+            let (IsInPreviousRowBounds, IsInCurrentRowBounds, IsInNextRowBounds) = getEdgedBounds(index, rows, columns)
+            seq {
+                for x:int in [-1; 0; 1] do 
+                    let kindex = index - columns + x //row below
+                    if IsInNextRowBounds (kindex) then
+                        yield kindex 
+
+                for x in [-1; 1] do 
+                    let kindex = index + x //current row
+                    if IsInCurrentRowBounds (kindex) then
+                        yield kindex 
+
+                for x in [-1; 0; 1] do 
+                    let kindex = index + columns + x //row above
+                    if IsInPreviousRowBounds (kindex) then      
+                        yield kindex 
+            }
+            
+        | Edgeless ->  
+            let cellCount = rows * columns
+            let indexBelow = index - columns
+            let indexAbove = index + columns      
+            let topRow = int (cellCount / columns) - 1
+            let currentRow = int (index / columns)
+            let rowBelow = 
+                if currentRow > 0 then
+                    currentRow - 1
+                else
+                    topRow
+            let rowAbove = 
+                if currentRow = topRow then
+                    0
+                else
+                    currentRow + 1
+            let currentColumn = index % columns
+            let columnLeft = 
+                if currentColumn > 0 then
+                    currentColumn - 1
+                else 
+                    columns - 1
+            let columnRight = 
+                if currentColumn < (columns - 1) then 
+                    currentColumn + 1
+                else
+                    0
+                    
+            let s = seq {
+                yield (rowAbove * columns) + columnLeft
+                yield (rowAbove * columns) + currentColumn
+                yield (rowAbove * columns) + columnRight
+                yield (currentRow * columns) + columnLeft
+                yield (currentRow * columns) + columnRight
+                yield (rowBelow * columns) + columnLeft
+                yield (rowBelow * columns) + currentColumn
+                yield (rowBelow * columns) + columnRight                                   
+            }
+            //printfn "Edgeless seq%s" (stringifyIntSeq ", " s)
+            s
+
+let updateKinCount(lifeBoard: LifeBoard, index, cellType, fGetKin) = 
     let n = 
         match cellType with
             | FullCell -> 1
             | EmptyCell -> -1
-    let rows = lifeBoard.dimensions.rows 
-    let columns = lifeBoard.dimensions.columns
-    let cellCount = rows * columns    
     
-    seq {
-        for x:int in [-1; 0; 1] do yield index - columns - x//previous row            
-        for x in [-1; 1] do 
-            let kindex = index + x
-            let kRow = (int) index / rows
-            let iRow = (int) kindex / rows
-            if kRow = iRow then            
-                yield index + x //current row
-        for x in [-1; 0; 1] do yield index + columns + x //next row
-    }
-    |> Seq.filter(fun ndx -> (ndx >= 0) && (ndx < cellCount) )    
-    |>  Seq.iter(fun cellIndex ->     
+    let s = fGetKin index        
+        
+    //printfn "updateKinCount:%s" (stringifyIntSeq ", " s)
+    s |>  Seq.iter(fun cellIndex ->     
         let (kinState, kinCount) = lifeBoard.lifeArray.[cellIndex]
         lifeBoard.lifeArray.[cellIndex] <- (kinState, kinCount + n)
     )
 
+let getKinFunc(lifeBoard: LifeBoard) = 
+    let rows = lifeBoard.dimensions.rows 
+    let columns = lifeBoard.dimensions.columns    
+    getKin rows columns lifeBoard.surfaceType
+    
+
 let updateCellStates(lifeBoard: LifeBoard) = 
+    let kinGet = getKinFunc(lifeBoard)  // this function could be stored on the board
+
     let ((_, changes), kList) = 
         lifeBoard.lifeArray |>     
         Array.fold(fun ((index, changeCount), kinList) ((_, currentState), kinCount)->
@@ -86,14 +177,14 @@ let updateCellStates(lifeBoard: LifeBoard) =
             ((index + 1, changeCount + change), newKinList)
         ) ((0, 0), List.empty<int * CellContents>)
 
-    kList |> List.iter(fun (index, newState) -> updateKinCount(lifeBoard, index, newState))
+    kList |> List.iter(fun (index, newState) -> updateKinCount(lifeBoard, index, newState, kinGet))
     changes
 
 
 let initialLifeBoard( dimensions: BoardDimensions) = 
     let cellCount = dimensions.rows * dimensions.columns
     let random = new System.Random()
-    let nextRandom () = random.Next(0, 99)
+    let nextRandom () = random.Next(0, cellCount - 1)
     let randomlist = [ for i in 1 .. cellCount -> nextRandom ()]
     let arr = 
         List.map(fun n -> 
@@ -102,11 +193,12 @@ let initialLifeBoard( dimensions: BoardDimensions) =
                 | _ -> CellDesert, 0
             ) randomlist
     let lifeArray = Array.ofList arr
-    let lifeBoard = {lifeArray = lifeArray; dimensions = {rows = dimensions.rows; columns = dimensions.columns}; changeFlag = true}
+    let lifeBoard = {lifeArray = lifeArray; dimensions = {rows = dimensions.rows; columns = dimensions.columns}; changeFlag = true; surfaceType = Edgeless}
+    let kinGet = getKinFunc(lifeBoard)
 
     lifeArray |> Array.fold(fun index (cellState, _kinCount)->
         (cellState = CellBirth) |> iter  (fun() ->
-            updateKinCount(lifeBoard, index, FullCell)            
+            updateKinCount(lifeBoard, index, FullCell, kinGet)            
         )                
         index + 1
     ) 0 |> ignore
@@ -132,14 +224,40 @@ let evolve(lifeBoard:LifeBoard) =
     let changes = updateCellStates(lifeBoard)
     {lifeBoard with changeFlag = (changes > 0);} 
 
+let contrivedLifeBoard(dimensions: BoardDimensions) = 
+    let cellCount = dimensions.rows * dimensions.columns
+    
+    let arr = 
+        seq { 
+            for index in 0 .. (cellCount - 1) do
+                let i = index % dimensions.columns 
+                if i = 0 || i = (dimensions.columns  - 1) then
+                    yield CellBirth, 0
+                else 
+                    yield CellDesert, 0
+        }
+    let lifeArray = Array.ofSeq arr
+    let lifeBoard = {lifeArray = lifeArray; dimensions = {rows = dimensions.rows; columns = dimensions.columns}; changeFlag = true; surfaceType = Edgeless}
+    let kinGet = getKinFunc(lifeBoard)
+    
+    lifeArray |> Array.fold(fun index (cellState, _kinCount)->
+        (cellState = CellBirth) |> iter  (fun() ->
+            updateKinCount(lifeBoard, index, FullCell, kinGet)            
+        )                
+        index + 1
+    ) 0 |> ignore
+    
+    lifeBoard
+    
+
 let f() = 
-    let lifeBoard:LifeBoard = initialLifeBoard({rows = 5; columns = 5})
+    //let lifeBoard:LifeBoard = initialLifeBoard({rows = 5; columns = 5})
+    let lifeBoard:LifeBoard = contrivedLifeBoard({rows = 5; columns = 5})
     renderBoard(lifeBoard)
     let c = updateCellStates(lifeBoard)
     let lifeBoard = {lifeBoard with changeFlag = (c > 0)}    
     renderBoard(lifeBoard)
+    let kinGet = getKinFunc(lifeBoard)
     
 
-
-
-
+    updateKinCount(lifeBoard, 0, FullCell, kinGet)
--- README.md
+++ README.md
@@ -1,3 +1,18 @@
-"# Life" 
+# Life
 
-Just started building this as a simple Fabulous app so ignore for now
+A fair bit more to do, but this version of John Conway's Life works (on Android)
+
+It uses F# / Fabulous
+
+Currently the Life board is not configurable though the hooks to do so are in place
+
+Life board size is fixed at a 10 * 10 grid
+
+Life board initialises with random start up seed.  Clicking the reset button reseeds the board.
+
+There are two 'playing' modes Edged and Edgeless.  In the Edged version a corner cell only has 3 neighbours, while  in the edgeless version all cells have 8 neighbours.
+
+When the timer is on, 'evolution' proceeds at 1 'frame' a second.
+
+When no changes occur, the timer stops (albeit not quite immediately - need to send cancellation).  Also the timer button doesn't not update when this happens so you may have to press it twice to start and stop.
+
